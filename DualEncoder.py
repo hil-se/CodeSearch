0a8518f69875692b71666c4525bf389888ce69c7
Monoshiz Mahbub Khan <mk7989@rit.edu>
2023-11-06 10:22:05 -0500

    Added contrastive loss

--- DualEncoder.py
+++ DualEncoder.py
@@ -9,6 +9,7 @@ from loss import weighted_cross_entropy, balanced_cross_entropy, balanced_l2, we
 import pandas as pd
 import random
 import math
+import tensorflow_addons as tfa
 
 def create_encoder(num_layers, input_size, output_size, dropout_rate):
     input = tf.keras.layers.Input(shape=(input_size,))
@@ -33,6 +34,8 @@ class DualEncoderAll(tf.keras.Model):
         self.target_encoder = target_encoder
         self.temperature = temperature
         self.compute_loss = self.compute_sim_loss
+        # self.compute_loss = self.compute_softmax_loss
+        # self.compute_loss = self.compute_contrastive_loss
         self.loss_tracker = tf.keras.metrics.Mean(name="loss")
 
     @property
@@ -74,6 +77,30 @@ class DualEncoderAll(tf.keras.Model):
         )
         # Return the mean of the loss over the batch.
         return (source_loss + target_loss) / 2
+
+    def compute_contrastive_loss(self, source_embeddings, target_embeddings):
+        logits = (
+                tf.matmul(source_embeddings, target_embeddings, transpose_b=True) / self.temperature
+        )
+        # target_similarity[i][j] is the dot_similarity(target_i, target_j).
+        target_similarity = tf.matmul(
+            target_embeddings, target_embeddings, transpose_b=True
+        )
+        # source_similarity[i][j] is the dot_similarity(source_i, source_j).
+        source_similarity = tf.matmul(
+            source_embeddings, source_embeddings, transpose_b=True
+        )
+        # targets[i][j] = avarage dot_similarity(source_i, source_j) and dot_similarity(target_i, target_j).
+        targets = tf.keras.activations.softmax(
+            (source_similarity + target_similarity) / (2 * self.temperature)
+        )
+        # ones = tf.ones(tf.shape(targets))
+        # targets = tf.math.subtract(ones, targets)
+        y_pred = tf.linalg.norm(source_embeddings - target_embeddings)
+        # Compute the contrastive loss
+        loss = tfa.losses.contrastive_loss(y_true=targets, y_pred=y_pred)
+        # Return the contrastive loss over the batch.
+        return loss
 
     def compute_softmax_loss(self, source_embeddings, target_embeddings):
         logits = (
@@ -168,56 +195,6 @@ class DualEncoderAll(tf.keras.Model):
         encoded_tensors.to_csv(filename, index=False)
         print(filename, "saved")
         return encoded_tensors["Source"].tolist(), encoded_tensors["Target"].tolist()
-
-    def generateBinaryClassificationData(self, feature, data_set="train"):
-        print("Generating binary classification data for", data_set, "set")
-        source_embeddings, target_embeddings = self(feature)
-        print("Data encoded\nGenerating links...")
-        sources = []
-        targets = []
-        for i in range(len(source_embeddings)):
-            thisTensor = source_embeddings[i]
-            if isInList(sources, thisTensor) == False:
-                sources.append(thisTensor)
-        for i in range(len(target_embeddings)):
-            thisTensor = target_embeddings[i]
-            if isInList(targets, thisTensor) == False:
-                targets.append(thisTensor)
-        links = []
-        ln = len(source_embeddings)
-        for i in range(ln):
-            s = source_embeddings[i]
-            t = target_embeddings[i]
-            s_ind = findIndex(sources, s)
-            t_ind = findIndex(targets, t)
-            links.append((s_ind, t_ind))
-        print("Generated links\nGenerating data...")
-        s_es = tf.split(source_embeddings, source_embeddings.shape[0], 0)
-        t_es = tf.split(target_embeddings, target_embeddings.shape[0], 0)
-        total_length = len(s_es)
-        results = []
-        for i in range(total_length):
-            if (i % 2 == 0 and i != 0) or i == (total_length - 1):
-                print("Progress:", i, "/", total_length)
-            s_e = ((tf.squeeze(s_es[i])).numpy())
-            t_e = ((tf.squeeze(t_es[i])).numpy())
-            pos_score = np.matmul(s_e, t_e)
-            results.append({"Score": pos_score, "Label": 1})
-            temp = []
-            while len(temp)<5:
-                idx = random.randint(0, total_length-1)
-                if idx!=i and idx not in temp:
-                    temp.append(idx)
-                    score = np.matmul(s_e, ((tf.squeeze(t_es[idx])).numpy()))
-                    if (i, idx) in links:
-                        results.append({"Score": score, "Label": 1})
-                    else:
-                        results.append({"Score": score, "Label": 0})
-        print("Data generated\nSaving to file...")
-        results_df = pd.DataFrame(results)
-        results_df.to_csv("Results/Tensors/ClassificationData_"+data_set+".csv")
-        print("Done\n\n")
-
 
     def test_retrieval(self, feature, k = 3):
         source_embeddings, target_embeddings = self(feature)
@@ -316,42 +293,3 @@ def getRank(listOfPairs, index, links):
         if index == targetIndex or (index, targetIndex) in links:
             return (i + 1)
     return 0
-
-#
-# def AdvTestMetrics(data, links):
-#     sources = data["Source"].tolist()
-#     targets = data["Target"].tolist()
-#     currentTensor = None
-#     currentSims = None
-#     RR = 0
-#     acc = 0
-#     total_length = len(sources)
-#     # total_length = 50
-#     start_time = time.time()
-#     abs_start_time = time.time()
-#     print("\nCalculating MRR score...\n")
-#     for i in range(total_length):
-#         if (i % 200 == 0 and i != 0) or i == (total_length - 1):
-#             end_time = time.time()
-#             print("Progress:", i, "/", total_length, ", Current MRR:", (RR / (i + 1)), ", Accuracy:", acc / (i + 1),
-#                   ", Time since last update:", (round(end_time - start_time, 3)), "s")
-#             start_time = time.time()
-#         currentTensor = sources[i]
-#         currentSims = []
-#         for j in range(total_length):
-#             currentSims.append((j, np.matmul(currentTensor, targets[j])))
-#         currentSims = sorted(currentSims, key=lambda x: float(x[1]), reverse=True)
-#         targetIndex = currentSims[0][0]
-#         if targetIndex == i or (i, targetIndex) in links:
-#             acc += 1
-#         rank = getRank(currentSims, i, links)
-#         if rank != 0:
-#             RR += (1 / (rank))
-#     mrr_score = RR / total_length
-#     acc = acc / total_length
-#     end_time = time.time()
-#     final_time = round(end_time - abs_start_time, 3)
-#     print("\nFinal MRR score:", mrr_score)
-#     print("Final accuracy:", acc)
-#     print("Total time taken:", final_time, "s")
-#     return mrr_score, acc, final_time
